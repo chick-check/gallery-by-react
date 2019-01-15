55b76d003326a649ca3d9f575045af82372d74a6
weichaofang <weichaofang@souche>
2019-01-15 14:54:59 +0800

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,10 +6,11 @@ require('styles/App.css');
 import React from 'react';
 
 //获取图片的json路径
-var imageDatas = require('../data/imageDatas.json');
+let imageDatas = require('../data/imageDatas.json');
 
 //let yeomanImage = require('../images/yeoman.png');
 
+/*****************************************图片url start******************************************/
 //获取图片url,写函数遍历所有图片,也可以用自执行函数
 function getImageURL(imageDatasArr){
   for (var i = 0;i<imageDatasArr.length; i++){
@@ -23,7 +24,45 @@ function getImageURL(imageDatasArr){
 //调用函数
 imageDatas = getImageURL(imageDatas);
 
-/* 注释默认代码
+/*****************************************图片url end******************************************/
+
+//单个图片的展示（图片以及图片下的文字）
+/*
+var imgFigure = React.createClass({
+  render: function(){
+    return(
+      <figure>
+        <img src = {this.props.data.imageURL} alt = {this.props.data.title}/>
+        <figcaption>
+          <h2>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+
+    );
+  }
+})
+*/
+/*****************************************单个图片样式处理start******************************************/
+//ES6 class 定义组件
+class ImgFigure extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <figure className = 'img-figure'>
+        <img src={this.props.data.imageURL} alt={this.props.data.title}/>
+        <figcaption>
+          <h2 className = 'img-title'>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+/*****************************************单个图片样式处理end******************************************/
+
+/*注释默认代码
 class AppComponent extends React.Component {
   render() {
     return (
@@ -35,19 +74,31 @@ class AppComponent extends React.Component {
   }
 }
 */
-//页面布局
+/*****************************************页面布局start******************************************/
 class AppComponent extends React.Component {
   render() {
+    //控制组件和图片数组
+    var controllerUnits = [],imgFigures = [];
+    //forEach() 方法用于调用数组的每个元素，并将元素传递给回调函数。
+    /*imageDatas.forEach(function(value){
+      imgFigures.push(<imgFigure data = {value}/>);
+    });*/
+    imageDatas.forEach((value,index)=>{
+      imgFigures.push(<ImgFigure data = {value} key = {index}/>);
+    });
     return (
-      <section className = "index">
+      <section className = "stage">
         <section className = 'image-sec'>
+         {imgFigures}
         </section>
         <nav className = 'controller-nav'>
+         {controllerUnits}
         </nav>
       </section>
     );
   }
 }
+/*****************************************页面布局end******************************************/
 
 AppComponent.defaultProps = {
 };
